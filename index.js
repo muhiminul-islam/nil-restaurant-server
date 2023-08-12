bf291d7b39e8f3d1e158ce1d247559a6b2f538fc
muhiminul-islam <islammuhiminul11@gmail.com>
2023-08-12 23:55:54 +0600

    carts item delete done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require("dotenv").config();
 const app = express();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -54,6 +54,13 @@ async function run() {
             const item = req.body;
             console.log(item);
             const result = await cartsCollection.insertOne(item);
+            res.send(result);
+        })
+
+        app.delete('/carts/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: new ObjectId(id)};
+            const result = await cartsCollection.deleteOne(query);
             res.send(result);
         })
 
